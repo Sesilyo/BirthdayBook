02c8d486eb687e46ea6891659a68ff41ecc1d906
Sesilyo <c.sethleander@gmail.com>
2025-04-15 12:04:00 +0800

    Made the log in component

---
+++ frontend/src/components/Log-In-Form.jsx
@@ -0,0 +1,18 @@
+
+function LogInForm()
+{
+    return (
+        <>
+        <form>
+            <p>Birthdaybook</p>
+            <label>Username</label><br/>
+            <input type="text"></input><br/>
+            <label>Email</label><br/>
+            <input type="email"></input><br/>
+            <input type="submit"></input>
+        </form>
+        </>
+    );
+}
+
+export default LogInForm
